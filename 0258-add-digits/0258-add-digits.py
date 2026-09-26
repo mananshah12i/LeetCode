24daaca71db4e466c1432074ea97ff04a10c5f9b
Manan Shah <mananshah12122006@gmail.com>
2026-09-26 23:58:31 +0530

    Time: 3 ms (21.95%), Space: 12.2 MB (90.28%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -4,4 +4,12 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        
+        l=len(str(num))
+        while l!=1:
+            s=0
+            p=str(num)
+            for i in range(l):
+                s+=int(p[i])
+            num=s
+            l=len(str(num))
+        return num
